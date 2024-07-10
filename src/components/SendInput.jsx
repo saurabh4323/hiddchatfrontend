b132c47e0f227504f7522603f86cdc6e478b3c13
saurabh singh <138216611+saurabh4323@users.noreply.github.com>
2024-07-10 11:29:22 +0530

    Update SendInput.jsx

--- src/components/SendInput.jsx
+++ src/components/SendInput.jsx
@@ -3,7 +3,7 @@ import { IoSend } from "react-icons/io5";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setMessages } from "../redux/messageSlice";
-import { BASE_URL } from "..";
+import { BASE_URL } from "../config";
 import "./SendInput.css"; // Import the CSS file
 
 const SendInput = () => {
